047c4681d50b282855934e5bba88947e11202414
Nicolay Kalinin <nikolajk202@gmail.com>
2023-08-23 19:42:19 +0300

    Initial commit

---
+++ src/test/java/ru/netology/springbootnginx/SpringBootNginxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.springbootnginx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNginxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
